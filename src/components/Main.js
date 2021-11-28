5daf6922e0af7067eb0fb77f6f2d84cd962e7366
KeitaUenishi <keita.uenishi@gmail.com>
2021-11-28 16:34:27 +0900

    main layout grid in

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles';
 
-export const Main = () => {
+import { MessageInputField } from './MessageInputField';
+import { MessageList } from './MessageList';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateRows: '1fr auto',
+  },
+});
+
+export const Main = ({name}) => {
+const classes = useStyles();
+
   return (
-    <div>
-      Main
+    <div className={classes.root}>
+      <MessageList />
+      <MessageInputField name={name}/>
     </div>
   )
 }
---
+++ src/components/MessageInputField.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MessageInputField = () => {
+  return (
+    <div>
+      MeesageInputField
+    </div>
+  )
+}
