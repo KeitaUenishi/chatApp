b44488424cc75ba8fbb3c7bee7f00b105560d23c
KeitaUenishi <keita.uenishi@gmail.com>
2021-11-28 14:09:30 +0900

    create json

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React , {useState} from 'react';
 import SignIn from './SignIn';
-import { Main } from './Main'
+import { Main } from './Main';
+import config from '../config.json';
 
 export default () => {
   const[name, setName] = useState('');
   console.log({name});
 
-  if (name === ''){
+  if (config.signInEnabled && name === ''){
     return <SignIn setName={setName}/>;
   }
   else{
