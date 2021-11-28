0adf2975352e2e2edd0df6a523411eacbfb8dbf3
KeitaUenishi <keita.uenishi@gmail.com>
2021-11-28 13:23:19 +0900

    name set enter

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React , {useState} from 'react';
 import SignIn from './SignIn';
+import { Main } from './Main'
 
 export default () => {
   const[name, setName] = useState('');
   console.log({name});
 
-  return <SignIn setName={setName}/>;
+  if (name === ''){
+    return <SignIn setName={setName}/>;
+  }
+  else{
+    return <Main name={name} />;
+  }
+
 };
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -46,6 +46,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
+  const [isComposed, setIsComposed] = useState(false)
 
   useEffect(() => {
     const disabled = string === '';
@@ -70,6 +71,16 @@ export default function SignIn({ setName }) {
             name="name"
             autoFocus
             onChange={(e) => setString(e.target.value)}
+            onKeyDown={(e) => {
+              if(isComposed) return;
+
+              if(e.key === 'Enter'){
+                setName(e.target.value);
+                e.preventDefault();
+              }
+            }}
+            onCompositionStart={() => {setIsComposed(true)}}
+            onCompositionEnd={() => {setIsComposed(false)}}
           />
           <Button
             type="button"
