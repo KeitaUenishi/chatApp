a2fd4a430b920c6b100f96f7e80789f5f76de083
KeitaUenishi <keita.uenishi@gmail.com>
2021-11-28 17:00:14 +0900

    inputField grid in

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Avatar, Grid} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+    margin: '26px',
+  },
+});
 
 export const MessageInputField = () => {
+  const classes = useStyles();
+  
   return (
-    <div>
-      MeesageInputField
+    <div className={classes.root}>
+      <Grid container>
+        <Grid xs={1}>
+          <Avatar />
+        </Grid>
+        <Grid xs={10}>入力</Grid>
+        <Grid xs={1}>ボタン</Grid>
+      </Grid>
     </div>
   )
 }
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,
+  },
+});
 
 export const MessageList = () => {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       MeesageList
     </div>
   )
