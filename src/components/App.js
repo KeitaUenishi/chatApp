a641b5af6c2a5ec9b68550fe3b52f22a3d5fa5fe
KeitaUenishi <keita.uenishi@gmail.com>
2022-01-04 11:40:18 +0900

    set route path

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,30 @@ import { Main } from './Main';
 import { SignUp } from './SignUp'
 import config from '../config.json';
 import { AuthProvider } from '../context/AuthContext';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 export default () => {
   const[name, setName] = useState('');
   const signIn = false; // 仮設定
   return(
     <AuthProvider>
-      {signIn ?
+      {/* {signIn ?
         config.signInEnabled && name === ''
         ? (<SignIn setName={setName}/>)
         : (<Main name={name} />)
       :
-        (<SignUp/>)
-      }
+        ( */}
+          <BrowserRouter>
+            <Route exact path="/" >
+              <Main name={name} />
+            </Route>
+            <Route path="/signup" component={SignUp} />
+            <Route path="/signin" >
+              <SignIn setName={setName} />
+            </Route>
+          </BrowserRouter>
+        {/* )
+      } */}
     </AuthProvider>
   )
 
