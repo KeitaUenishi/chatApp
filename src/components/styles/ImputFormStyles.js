0e152cfaebf44e059cb2e5a07825832442f5ba48
KeitaUenishi <keita.uenishi@gmail.com>
2022-11-10 22:09:39 +0900

    settings firebase connection

--- src/components/styles/ImputFormStyles.js
+++ src/components/styles/ImputFormStyles.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-
+import { makeStyles } from "@material-ui/core/styles";
 
 export const ImputFormStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(10),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
-  text : {
-    textAlign: 'center',
+  text: {
+    textAlign: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,33 @@
-
 import firebase from "firebase";
-import 'firebase/auth'
+import "firebase/auth";
 
 const {
   REACT_APP_FIREBASE_API_KEY,
   REACT_APP_FIREBASE_AUTH_DOMAIN,
   REACT_APP_FIREBASE_PROJECT_ID,
-  REACT_APP_FIREBASE_STRAGE_BUCKET,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
   REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_IDz,
-} =  process.env;
+  REACT_APP_FIREBASE_APP_ID,
+} = process.env;
 
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_API_KEY,
   authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STRAGE_BUCKET,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_IDz,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-export const messagesRef = database.ref('messages');
+export const messagesRef = database.ref("messages");
 
 export const pushMessage = ({ name, text }) => {
   messagesRef.push({ name, text });
-}
+};
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 
